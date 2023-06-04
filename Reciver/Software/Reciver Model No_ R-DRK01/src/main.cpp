1754ca1e35b96ef3c1468c03c8a51b124aaefa42
Efkan Güvenir <efkanguvenir@gmail.com>
2023-06-04 13:25:06 +0300

    toplu düzenleme

--- Reciver/Software/Reciver Model No_ R-DRK01/src/main.cpp
+++ Reciver/Software/Reciver Model No_ R-DRK01/src/main.cpp
@@ -1,4 +1,4 @@
-//*----V1.0 (Beta_3)----*//
+//*---- DRK01 V1.0  (Beta_4)  ----*//
 
 #include <Arduino.h>
 #include <VirtualWire.h> //433RF library
@@ -8,52 +8,44 @@ const int RF_module_pin = 11; // 433mhz Alıcının Bağlı Olduğu Pin (interru
 const int otomatik_led = 12;  // Otomatik Söküm Sisteme Girdiğinde bildirim ışığı
 const int sokum_led = 13;     // Söküm Sisteme Girdiğinde bildirim ışığı
 const int RX_data_led = 14;   // RF'den Gelen Veri Olduğunda Bildirilecek led
+const int Switch1 = 27;       // Şifreleme Anahtarının 1. biti
+const int Switch2 = 26;       // Şifreleme Anahtarının 2. biti
+const int Switch3 = 25;       // Şifreleme Anahtarının 3. biti
+const int Switch4 = 34;       // Şifreleme Anahtarının 4. biti
 
 //* değişken
 uint8_t rf_digit_key = 0; // 4 Bit Şifreleme için değişken değeri
 bool yetki = false;       // Kumanda Şifrelemede Yetkilendirme Değişkeni
 bool flag = false;        // Tuşa basılı tuttuğu sürece saçmalamaması için
-int bit_1 = 0;            // Gönderilen Değişken Bitlerden 1.
-int bit_2 = 0;            // Gönderilen Değişken Bitlerden 2.
-int bit_3 = 0;            // Gönderilen Değişken Bitlerden 3.
+
+bool bit7, bit6, bit5, bit4, bit3, bit2, bit1, bit0;         // Mainboard'a Gidecek Birinci Veri
+bool bit15, bit14, bit13, bit12, bit11, bit10, bit9, bit8;   // Mainboard'a Gidecek  İkinci Veri
+bool bit23, bit22, bit21, bit20, bit19, bit18, bit17, bit16; // Mainboard'a Gidecek  Üçüncü Veri
 
 //* ON OFF Sistem için
 bool otomatik_aktif = false; // On-Off İçin Değişlen
 bool sokum_aktif = false;    // On-Off İçin Değişlen
 bool lamba_aktif = false;    // On-Off İçin Değişlen
 
-//* MainBoard'a Gönderilecek DEC Kumanda Kodlar
-const int otomatik_send_DEC_cod = 1;           // Otomatik Tuşunun DEC Kodu
-const int sokum_send_DEC_cod = 32;             // Söküm Tuşunun DEC Kodu
-const int sokucu_yukari_send_DEC_cod = 4;      // Sökücü Yukarı Tuşunun DEC Kodu
-const int sokucu_asagi_send_DEC_cod = 2;       // Sökücü Aşağı Tuşunun DEC Kodu
-const int sokucu_sag_send_DEC_cod = 4;         // Sökücü Sağa Tuşunun DEC Kodu
-const int sokucu_sol_send_DEC_cod = 2;         // Sökücü Sola Tuşunun DEC Kodu
-const int depo_yukari_send_DEC_cod = 8;        // Depo Yukarı Tuşunun DEC Kodu
-const int depo_asagi_send_DEC_cod = 16;        // Depo Aşağı Tuşunun DEC Kodu
-const int hazirlayici_yukari_send_DEC_cod = 8; // Hazırlayıcı Yukarı Tuşunun DEC Kodu
-const int hazirlayici_asagi_send_DEC_cod = 16; // Hazırlayıcı Aşağı Tuşunun DEC Kodu
-const int ilave_ac_send_DEC_cod = 1;           // İlave Aç Tuşunun DEC Kodu
-const int ilave_kapat_send_DEC_cod = 128;      // İlave Kapat Tuşunun DEC Kodu
-const int bosalt_calis_send_DEC_cod = 32;      // Boşaltma Çalıştır Tuşunun DEC Kodu
-const int bosalt_dur_send_DEC_cod = 64;        // Boşaltma Durdur Tuşunun DEC Kodu
-const int lamba_send_DEC_cod = 64;             // Lamba Tuşunun DEC Kodu
-const int sistem_geri_send_DEC_cod = 128;      // Sistem Geri Tuşunun DEC Kodu
-
 void rf_data(String data) //* Gelen Datanın Ayıklanması
 {
   if (data == "0") //* Tuş Bırakıldığında
   {
+    bit0 = false, bit1 = false, bit2 = false, bit3 = false, bit4 = false, bit5 = false, bit6 = false, bit7 = false;
+    bit8 = false, bit9 = false, bit10 = false, bit11 = false, bit12 = false, bit13 = false, bit14 = false, bit15 = false;
+    bit16 = false, bit17 = false, bit18 = false, bit19 = false, bit20 = false, bit21 = false, bit22 = false, bit23 = false;
     flag = false; // Flag'ı Pasifleştir
-    bit_1 = 0;
-    bit_2 = 0;
-    bit_3 = 0;
     if (otomatik_aktif == true)
-      bit_1 = bit_1 + otomatik_send_DEC_cod;
+    {
+      bit0 = true;
+    }
     if (sokum_aktif == true)
-      bit_2 = bit_2 + sokum_send_DEC_cod;
+    {
+      bit13 = true;
+    }
     if (lamba_aktif == true)
-      bit_2 = bit_2 + lamba_send_DEC_cod;
+    {
+    }
   }
 
   if ((flag == false) && (data != "0")) // işlemler birdan fazla yapılmaması için
@@ -63,11 +55,13 @@ void rf_data(String data) //* Gelen Datanın Ayıklanması
       if (otomatik_aktif) // Otomatik Aktif ise
       {
         otomatik_aktif = false; // Otomatiği Kapat
+        bit0 = false;           // Otomatiği Kapat
         digitalWrite(otomatik_led, LOW);
       }
       else // Otomatik Aktif Değilse
       {
         otomatik_aktif = true;
+        bit0 = true;
         digitalWrite(otomatik_led, HIGH);
       }
     }
@@ -77,11 +71,13 @@ void rf_data(String data) //* Gelen Datanın Ayıklanması
       if (sokum_aktif)
       {
         sokum_aktif = false;
+        bit13 = false;
         digitalWrite(sokum_led, LOW);
       }
       else
       {
         sokum_aktif = true;
+        bit13 = true;
         digitalWrite(sokum_led, HIGH);
       }
     }
@@ -93,45 +89,47 @@ void rf_data(String data) //* Gelen Datanın Ayıklanması
       else
         lamba_aktif = true;
     }
-
     if (data == "3") //* Sökücü Yukarı
-      bit_2 = bit_2 + sokucu_yukari_send_DEC_cod;
+      bit10 = true;
 
     if (data == "4") //* Sökücü Aşağı
-      bit_2 = bit_2 + sokucu_asagi_send_DEC_cod;
+      bit9 = true;
 
-    if (data == "5") //* Sökücü Sağa
-      bit_1 = bit_1 + sokucu_sag_send_DEC_cod;
+    if (data == "17") //* Sökücü Sağa
+      bit2 = true;
 
     if (data == "6") //* Sökücü Sola
-      bit_1 = bit_1 + sokucu_sol_send_DEC_cod;
+      bit1 = true;
 
     if (data == "7") //* Depo Yukarı
-      bit_1 = bit_1 + depo_yukari_send_DEC_cod;
+      bit8 = true;
 
     if (data == "8") //* Depo Aşağı
-      bit_1 = bit_1 + depo_asagi_send_DEC_cod;
+      bit7 = true;
 
     if (data == "9") //* Hazırlayıcı Yukarı
-      bit_2 = bit_2 + hazirlayici_yukari_send_DEC_cod;
+      bit11 = true;
 
     if (data == "10") //* Hazırlayıcı Aşağı
-      bit_2 = bit_2 + hazirlayici_asagi_send_DEC_cod;
+      bit12 = true;
 
     if (data == "11") //* İlave Kapatma
-      bit_1 = bit_1 + ilave_kapat_send_DEC_cod;
+      bit8 = true;
 
     if (data == "12") //* İlave açma
-      bit_2 = bit_2 + ilave_ac_send_DEC_cod;
+      bit7 = true;
 
     if (data == "13") //* Boşalt Çalıştır
-      bit_1 = bit_1 + bosalt_calis_send_DEC_cod;
+      bit5 = true;
 
     if (data == "14") //* Boşalt Durdur
-      bit_1 = bit_1 + bosalt_dur_send_DEC_cod;
-
+    {
+      // todo BoşaltDurdur bırakıldığında boşaltma devreye tekrar girdiğinden bir kod daha gönderiyor
+      bit6 = true; // Sistem Geri
+      bit4 = true; // Bosalt DurDur Kodu
+    }
     if (data == "16") //* Sistem Geri
-      bit_2 = bit_2 + sistem_geri_send_DEC_cod;
+      bit6 = true;
 
     flag = true; // Flag'ı Etkinleştir
   }
@@ -139,13 +137,37 @@ void rf_data(String data) //* Gelen Datanın Ayıklanması
 
 void setup()
 {
-  Serial.begin(1200);            // Seri Monitör
-  vw_set_rx_pin(RF_module_pin);  // 433mhz Modül Pin'i Belirlenmesi
-  vw_setup(1000);                // Saniye/Bit
-  vw_rx_start();                 // Start the receiver PLL running
+  Serial.begin(1200);           // Seri Monitör
+  vw_set_rx_pin(RF_module_pin); // 433mhz Modül Pin'i Belirlenmesi
+  vw_setup(1000);               // Saniye/Bit
+  vw_rx_start();                // Start the receiver PLL running
+
+  //* Pin Output input
+  pinMode(Switch1, INPUT);
+  pinMode(Switch2, INPUT);
+  pinMode(Switch3, INPUT);
+  pinMode(Switch4, INPUT);
   pinMode(RX_data_led, OUTPUT);  // Pin'i Çıkış Olarak Belirle
   pinMode(otomatik_led, OUTPUT); // Pin'i Çıkış Olarak Belirle
   pinMode(sokum_led, OUTPUT);    // Pin'i Çıkış Olarak Belirle
+
+  if (digitalRead(Switch1) == HIGH)
+  {
+    rf_digit_key = rf_digit_key + 1;
+  }
+  if (digitalRead(Switch2) == HIGH)
+  {
+    rf_digit_key = rf_digit_key + 3;
+  }
+  if (digitalRead(Switch3) == HIGH)
+  {
+    rf_digit_key = rf_digit_key + 5;
+  }
+  if (digitalRead(Switch4) == HIGH)
+  {
+    rf_digit_key = rf_digit_key + 7;
+  }
+  rf_digit_key = 0;
 }
 
 void loop()
@@ -174,14 +196,40 @@ void loop()
     }
     digitalWrite(RX_data_led, LOW); // Verinin Bittiğini Belirten Led
   }
-
+  byte binaryData_1 = 0;
+  binaryData_1 |= bit0;
+  binaryData_1 |= bit1 << 1;
+  binaryData_1 |= bit2 << 2;
+  binaryData_1 |= bit3 << 3;
+  binaryData_1 |= bit4 << 4;
+  binaryData_1 |= bit5 << 5;
+  binaryData_1 |= bit6 << 6;
+  binaryData_1 |= bit7 << 7;
+  byte binaryData_2 = 0;
+  binaryData_2 |= bit8;
+  binaryData_2 |= bit9 << 1;
+  binaryData_2 |= bit10 << 2;
+  binaryData_2 |= bit11 << 3;
+  binaryData_2 |= bit12 << 4;
+  binaryData_2 |= bit13 << 5;
+  binaryData_2 |= bit14 << 6;
+  binaryData_2 |= bit15 << 7;
+  byte binaryData_3 = 0;
+  binaryData_3 |= bit16;
+  binaryData_3 |= bit17 << 1;
+  binaryData_3 |= bit18 << 2;
+  binaryData_3 |= bit19 << 3;
+  binaryData_3 |= bit20 << 4;
+  binaryData_3 |= bit21 << 5;
+  binaryData_3 |= bit22 << 6;
+  binaryData_3 |= bit23 << 7;
   //* Mainboard'a Gönderilen Veri
   Serial.write(68);
   Serial.write(84);
   Serial.write(89);
-  Serial.write(bit_1);
-  Serial.write(bit_2);
-  Serial.write(bit_3);
+  Serial.write(binaryData_1);
+  Serial.write(binaryData_2);
+  Serial.write(binaryData_3);
 
   delay(59); // Bekleme Süresi (Değiştirilebilir)
 }

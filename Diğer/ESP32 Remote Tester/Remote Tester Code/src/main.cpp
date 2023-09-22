1da55c395d5c06be728ee85cd98aed3486801bd2
Efkan Güvenir <efkanguvenir@gmail.com>
2023-09-22 12:22:50 +0300

    bayt 3 Kaldırıldı

--- Diğer/ESP32 Remote Tester/Remote Tester Code/src/main.cpp
+++ Diğer/ESP32 Remote Tester/Remote Tester Code/src/main.cpp
@@ -10,13 +10,12 @@ BluetoothSerial SerialBT;
 
 /**************************************************************/
 //* Değişkenler
-bool bit7 = false, bit6 = false, bit5 = false, bit4 = false, bit3 = false, bit2 = false, bit1 = false, bit0 = false;         // Mainboard'a Gidecek Birinci Veri
-bool bit15 = false, bit14 = false, bit13 = false, bit12 = false, bit11 = false, bit10 = false, bit9 = false, bit8 = false;   // Mainboard'a Gidecek  İkinci Veri
-bool bit23 = false, bit22 = false, bit21 = false, bit20 = false, bit19 = false, bit18 = false, bit17 = false, bit16 = false; // Mainboard'a Gidecek  Üçüncü Veri
+bool bit7 = false, bit6 = false, bit5 = false, bit4 = false, bit3 = false, bit2 = false, bit1 = false, bit0 = false;       // Mainboard'a Gidecek Birinci Veri
+bool bit15 = false, bit14 = false, bit13 = false, bit12 = false, bit11 = false, bit10 = false, bit9 = false, bit8 = false; // Mainboard'a Gidecek  İkinci Veri
 
 byte binaryData_1 = 0;
 byte binaryData_2 = 0;
-byte binaryData_3 = 0;
+
 /**************************************************************/
 void setup()
 {
@@ -103,38 +102,6 @@ void loop()
       bit15 = true;
       SerialBT.println("bit15 = true - Byte2=128");
       break;
-    case 17:
-      bit16 = true;
-      SerialBT.println("bit16 = true - Byte3=1");
-      break;
-    case 18:
-      bit17 = true;
-      SerialBT.println("bit17 = true - Byte3=2");
-      break;
-    case 19:
-      bit18 = true;
-      SerialBT.println("bit18 = true - Byte3=4");
-      break;
-    case 20:
-      bit18 = true;
-      SerialBT.println("bit19 = true - Byte3=8");
-      break;
-    case 21:
-      bit20 = true;
-      SerialBT.println("bit10 = true - Byte3=16");
-      break;
-    case 22:
-      bit21 = true;
-      SerialBT.println("bit21 = true - Byte3=32");
-      break;
-    case 23:
-      bit22 = true;
-      SerialBT.println("bit22 = true - Byte3=64");
-      break;
-    case 24:
-      bit23 = true;
-      SerialBT.println("bit23 = true - Byte3=128");
-      break;
     case 30:
       SerialBT.println("ESP Resset!");
       delay(200);
@@ -149,13 +116,11 @@ void loop()
       SerialBT.print(binaryData_2);
       SerialBT.print("/DEC");
       SerialBT.print(" Byte3= ");
-      SerialBT.print(binaryData_3);
       SerialBT.println("/DEC");
       break;
     case 32:
       bit0 = false, bit1 = false, bit2 = false, bit3 = false, bit4 = false, bit5 = false, bit6 = false, bit7 = false;
       bit8 = false, bit9 = false, bit10 = false, bit11 = false, bit12 = false, bit13 = false, bit14 = false, bit15 = false;
-      bit16 = false, bit17 = false, bit18 = false, bit19 = false, bit20 = false, bit21 = false, bit22 = false, bit23 = false;
       SerialBT.println("bit all false");
       break;
     }
@@ -179,21 +144,13 @@ void loop()
   binaryData_2 |= bit13 << 5;
   binaryData_2 |= bit14 << 6;
   binaryData_2 |= bit15 << 7;
-  binaryData_3 = 0;
-  binaryData_3 |= bit16;
-  binaryData_3 |= bit17 << true;
-  binaryData_3 |= bit18 << 2;
-  binaryData_3 |= bit19 << 3;
-  binaryData_3 |= bit20 << 4;
-  binaryData_3 |= bit21 << 5;
-  binaryData_3 |= bit22 << 6;
-  binaryData_3 |= bit23 << 7;
+
   //* Mainboard'a Gönderilen Veri
   Serial2.write(68);
   Serial2.write(84);
   Serial2.write(89);
   Serial2.write(binaryData_1);
   Serial2.write(binaryData_2);
-  Serial2.write(binaryData_3);
+
   delay(60);
 }
